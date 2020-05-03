be32a052c60fa9003530ce8090bf013b59c92738
umairx97 <umair.bajwa@hotmail.com>
2020-05-03 18:50:49 +0500

    build: publish

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class TabNav extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      tabData: this.props.data
+      tabData: this.props.data || []
     }
 
     this.setItem = this.setItem.bind(this)
@@ -26,9 +26,10 @@ class TabNav extends Component {
   render () {
     const { activeTabStyles, tabBarStyles, classes, tabBarBorderStyle } = this.props
     const { tabData } = this.state
+    if (!tabData.length) return <div>no data given</div>
     return (
-      <div className='flexer-column container-fluid'>
-        <div className='row w-100' style={tabBarBorderStyle}>
+      <div className='container-fluid flexer-column'>
+        <div className='w-100 flexer' style={tabBarBorderStyle}>
           {tabData.map((item, index) => (
             <div
               key={index}
@@ -36,7 +37,7 @@ class TabNav extends Component {
               className={`${classes} col-lg-2 col-md-12 col-sm-12 flexer p-3`}
               onClick={() => this.setItem(index)}
             >
-              {item.tabName}
+              {item.tabName} this is updated
             </div>
           ))}
         </div>
