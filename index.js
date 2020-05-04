71f54c8ff6e8ab64b1a6a6f63c31a13c95e902db
umairx97 <umair.bajwa@hotmail.com>
2020-05-04 17:06:14 +0500

    feat: changed active styling issue and added proptypes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import React, { Component, Fragment } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Proptypes from 'prop-types'
+
 class TabNav extends Component {
   constructor (props) {
     super(props)
@@ -11,7 +13,7 @@ class TabNav extends Component {
   }
 
   setItem (index) {
-    const merged = this.state.tabData
+    const merged = [...this.state.tabData]
 
     if (!merged[index].isActive) {
       merged[index].isActive = !this.state.tabData[index].isActive
@@ -26,15 +28,15 @@ class TabNav extends Component {
   render () {
     const { activeTabStyles, tabBarStyles, classes, tabBarBorderStyle } = this.props
     const { tabData } = this.state
-    if (!tabData.length) return <div>no data given</div>
+    if (!tabData.length) return <div>Please Provide Data</div>
     return (
       <div className='container-fluid flexer-column'>
         <div className='w-100 flexer' style={tabBarBorderStyle}>
           {tabData.map((item, index) => (
             <div
               key={index}
-              style={(item.isActive && activeTabStyles) || tabBarStyles}
-              className={`${classes} col-lg-2 col-md-12 col-sm-12 flexer p-3`}
+              style={(item.isActive ? { ...tabBarStyles, ...activeTabStyles } : { ...tabBarStyles })}
+              className={`${classes} w-100 flexer`}
               onClick={() => this.setItem(index)}
             >
               {item.tabName} this is updated
@@ -56,6 +58,14 @@ class TabNav extends Component {
       </div>
     )
   }
+}
+
+TabNav.proptypes = {
+  activeTabStyles: Proptypes.object,
+  tabBarStyles: Proptypes.object,
+  classes: Proptypes.string,
+  tabBarBorderStyle: Proptypes.object,
+  data: Proptypes.array
 }
 
 export default TabNav
